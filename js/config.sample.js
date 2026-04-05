cc687b9695f2a486c04ae7550eb178cced2967a3
gilbarusa <gilbarzeski@gmail.com>
2026-04-05 15:29:17 -0400

    Delete js/config.sample.js

--- js/config.sample.js
+++
@@ -1,6 +0,0 @@
-// Configuration - Copy to config.js and fill in your keys
-const CONFIG = {
-  SUPABASE_URL: 'https://YOUR_PROJECT.supabase.co',
-  SUPABASE_KEY: 'YOUR_SUPABASE_ANON_KEY',
-  ADMIN_TOKEN: 'YOUR_ADMIN_TOKEN'
-};
