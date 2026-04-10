f585062499ab5960c15eb7d5b863b3f83d2f2c7d
gilbarusa <gilbarzeski@gmail.com>
2026-04-10 13:29:00 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,6 @@
 const CONFIG = {
   SUPABASE_URL: 'https://iwohrvkcodqvyoooxzmt.supabase.co',
   SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3b2hydmtjb2Rxdnlvb294em10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyOTM3OTksImV4cCI6MjA4OTg2OTc5OX0.PhKo6XiXf-VTBWcYkhT_vfHi0ibftNmYaqm4RApxO6Y',
-  ADMIN_TOKEN: 'willow2026!'
+  ADMIN_TOKEN: 'willow2026!',
+  DL_API: 'https://app.willowpa.com/portal/delivery/api.php'
 };
